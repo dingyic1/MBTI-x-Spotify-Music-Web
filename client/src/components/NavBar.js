0786d3a5e52957f3c40b4e4c0ae0669bce399dd2
Erica Sun <ericasun@seas.upenn.edu>
2023-04-30 16:54:22 -0400

    change font, background and color

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,55 +1,18 @@
-// import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-// import { NavLink } from "react-router-dom";
-
-// // The hyperlinks in the NavBar contain a lot of repeated formatting code so a
-// // helper component NavText local to the file is defined to prevent repeated code.
-// const NavText = ({ href, text, isMain }) => {
-//   return (
-//     <Typography
-//       variant={isMain ? "h5" : "h7"}
-//       noWrap
-//       style={{
-//         marginRight: "30px",
-//         fontFamily: "monospace",
-//         fontWeight: 700,
-//         letterSpacing: ".3rem",
-//       }}
-//     >
-//       <NavLink
-//         to={href}
-//         style={{
-//           color: "inherit",
-//           textDecoration: "none",
-//         }}
-//       >
-//         {text}
-//       </NavLink>
-//     </Typography>
-//   );
-// };
-
-// // Here, we define the NavBar. Note that we heavily leverage MUI components
-// // to make the component look nice. Feel free to try changing the formatting
-// // props to how it changes the look of the component.
-// export default function NavBar() {
-//   return (
-//     <AppBar position="static">
-//       <Container maxWidth="xl">
-//         <Toolbar disableGutters>
-//           <NavText href="/" text="Music x MBTI" isMain />
-//           {/* <NavText href="/albums" text="ALBUMS" />
-//           <NavText href="/songs" text="SONGS" />
-//           <NavText href="/artists" text="ARTISTS" /> */}
-//         </Toolbar>
-//       </Container>
-//     </AppBar>
-//   );
-// }
-
 import { useState } from "react";
-import { Drawer, List, ListItem, ListItemText, AppBar, Container, Toolbar, Typography, IconButton } from "@mui/material";
-import { Menu } from '@mui/icons-material';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
+import "../styles/stylesheet.css";
 
 // The hyperlinks in the NavBar contain a lot of repeated formatting code so a
 // helper component NavText local to the file is defined to prevent repeated code.
@@ -58,12 +21,13 @@ const NavText = ({ href, text, isMain }) => {
     <Typography
       variant={isMain ? "h5" : "h7"}
       noWrap
-      style={{
-        marginRight: "30px",
-        fontFamily: "monospace",
-        fontWeight: 700,
-        letterSpacing: ".3rem",
-      }}
+      // style={{
+      //   marginRight: "30px",
+      //   fontFamily: "monospace",
+      //   fontWeight: 700,
+      //   letterSpacing: ".3rem",
+      // }}
+      className="font-link"
     >
       <NavLink
         to={href}
@@ -89,29 +53,61 @@ export default function NavBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ background: "#8C52FF" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={toggleDrawer}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
+          >
             <Menu />
           </IconButton>
           <NavText href="/" text="Music x MBTI" isMain />
         </Toolbar>
       </Container>
       <Drawer anchor="left" open={showDrawer} onClose={toggleDrawer}>
-        <List style={{ backgroundColor: "#f5f5f5", height: "100%", fontFamily: "monospace",
-                       fontWeight: 700 }}>
-          <ListItem button component={NavLink} to="/albums" onClick={toggleDrawer}>
+        <List
+          style={{
+            backgroundColor: "#f5f5f5",
+            height: "100%",
+            fontFamily: "monospace",
+            fontWeight: 700,
+          }}
+        >
+          <ListItem
+            button
+            component={NavLink}
+            to="/albums"
+            onClick={toggleDrawer}
+          >
             <ListItemText primary="1. Albums" />
           </ListItem>
-          <ListItem button component={NavLink} to="/songs" onClick={toggleDrawer}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/songs"
+            onClick={toggleDrawer}
+          >
             <ListItemText primary="2. Search Songs" />
           </ListItem>
-          <ListItem button component={NavLink} to="/artists" onClick={toggleDrawer}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/artists"
+            onClick={toggleDrawer}
+          >
             <ListItemText primary="3. Artists" />
           </ListItem>
         </List>
-        <IconButton edge="start" color="inherit" aria-label="back" IconButton="close">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="back"
+          IconButton="close"
+        >
           <Menu onClick={toggleDrawer} />
         </IconButton>
       </Drawer>
--- client/src/pages/EntryPage.js
+++ client/src/pages/EntryPage.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Container, Divider, Link, Typography, Box, TextField, Button, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Container,
+  Divider,
+  Link,
+  Typography,
+  Box,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import "../styles/stylesheet.css";
 
 function EntryPage() {
-  const [mbti, setMbti] = useState('');
+  const [mbti, setMbti] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
@@ -16,13 +26,13 @@ function EntryPage() {
   };
 
   return (
-    <Container>
-      <Box sx={{ display: 'flex' }}>
+    <Container className="entry-page">
+      <Box sx={{ display: "flex" }}>
         <Grid container spacing={2}>
           <Grid item md={8}>
             <Box>
               <form onSubmit={handleSubmit}>
-                <Typography variant="h3" gutterBottom>
+                <Typography variant="h3" gutterBottom className="font-link">
                   Welcome to the MBTI x Music system!
                 </Typography>
                 <TextField
@@ -41,14 +51,16 @@ function EntryPage() {
             </Box>
           </Grid>
           <Grid item md={4}>
-            <Box sx={{ bgcolor: 'primary.light', p: 2 }}>
+            <Box sx={{ bgcolor: "primary.light", p: 2 }}>
               <Typography variant="h6" gutterBottom>
                 About MBTI
               </Typography>
               <Typography variant="body1">
-                The Myers-Briggs Type Indicator (MBTI) is a personality assessment tool used to measure personality
-                traits and preferences. It is based on Carl Jung's theory of psychological types and is widely used in
-                business, education, and other fields to improve communication and team dynamics.
+                The Myers-Briggs Type Indicator (MBTI) is a personality
+                assessment tool used to measure personality traits and
+                preferences. It is based on Carl Jung's theory of psychological
+                types and is widely used in business, education, and other
+                fields to improve communication and team dynamics.
               </Typography>
             </Box>
           </Grid>
