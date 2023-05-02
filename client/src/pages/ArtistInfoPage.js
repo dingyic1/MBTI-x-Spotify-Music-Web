10d27e214a9b3405a84ec73d979a7f928029b0fd
easonding <easonding1@outlook.com>
2023-05-02 09:16:00 -0400

    Change Artist ArtistInfoPage

--- client/src/pages/ArtistInfoPage.js
+++ client/src/pages/ArtistInfoPage.js
@@ -9,49 +9,47 @@ export default function ArtistInfoPage() {
 
   const [similarArtists, setSimilarArtists] = useState([]);
   const [artistData, setArtistData] = useState([]);
-  const [error, setError] = useState(null);
+  const [showSimilarArtists, setShowSimilarArtists] = useState(false);
 
   useEffect(() => {
     fetch(
-        `http://${config.server_host}:${config.server_port}/artist/${artist_id}`)
-        .then((res) => res.json())
-        .then((resJson) => setArtistData(resJson));
-    
-    fetch(`http://${config.server_host}:${config.server_port}/artists/similar/${artist_id}`)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Error fetching artist data");
-        }
-        return res.json();
-      })
-      .then((resJson) => {
-        console.log(artist_id);
-        setSimilarArtists(resJson);
-      })
-      .catch((err) => {
-        console.error(err);
-        setError(err.message);
-      });
+      `http://${config.server_host}:${config.server_port}/artist/${artist_id}`
+    )
+      .then((res) => res.json())
+      .then((resJson) => setArtistData(resJson));
   }, [artist_id]);
+
+  const handleSimilarArtistsClick = () => {
+    fetch(
+      `http://${config.server_host}:${config.server_port}/artists/similar/${artist_id}`
+    )
+      .then((res) => res.json())
+      .then((resJson) => {
+        setSimilarArtists(resJson);
+        setShowSimilarArtists(true);
+      });
+  };
 
   return (
     <Container className="custom-font">
-      <h2>
-        Artist:&nbsp;
-        <NavLink>
-          {artistData.artist_name}
-        </NavLink>
-      </h2>
-      <h3>Similar Artists:</h3>
-      <ul>
-        {similarArtists.map((artist) => (
-          <li>
-            <NavLink to={`/artist/${artist.artist_id}`}>
-                {artist.artist_name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+      <h2>Artist:{artistData.artist_name}</h2>
+      <ButtonGroup>
+        <Button onClick={handleSimilarArtistsClick}>Search Similar Artists</Button>
+      </ButtonGroup>
+      {showSimilarArtists && (
+        <>
+          <h3>Similar Artists:</h3>
+          <ul>
+            {similarArtists.map((artist) => (
+              <li key={artist.artist_id}>
+                <NavLink to={`/artist/${artist.artist_id}`}>
+                  {artist.artist_name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </Container>
   );
 }
